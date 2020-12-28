310420a25921791b236218a1fcec2bca99f1d62e
DhareppaS1 <DhareppaS1@verifone.com>
2020-12-28 09:35:58 +0530

    commits

--- src/main/java/com/org/RJSON_File_Handling/File_Test.java
+++ src/main/java/com/org/RJSON_File_Handling/File_Test.java
@@ -1,4 +1,28 @@
 package com.org.RJSON_File_Handling;
 
+import java.io.*;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class File_Test {
+
+    public static void main(String[] args) {
+        loadFile("./config");
+    }
+
+    private static void loadFile(String s) {
+        File file = new File(s + "/key-val.json");
+        String filePath = file.getAbsolutePath();
+        String line;
+        Map<String, String> map = null;
+        try {
+            map = new ObjectMapper().readValue(filePath, Map.class);
+
+        } catch (Exception ignored) {
+            //
+        }
+        System.out.println(map);
+    }
 }
--- src/main/java/com/org/basics/test.java
+++ src/main/java/com/org/basics/test.java
@@ -1,4 +1,27 @@
 package com.org.basics;
 
+import java.io.*;
+import java.util.Objects;
+
 public class test {
+
+    public static void main(String[] args) {
+        loadFile("./config");
+    }
+
+    private static void loadFile(String s) {
+        File file = new File(s+ "/abc.txt");
+        String filePath = file.getAbsolutePath();
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+            while (Objects.nonNull(line = br.readLine())){
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
 }
